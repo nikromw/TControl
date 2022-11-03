6c179327a797d9970bfbe494179e8632ae3351e5
Nikitin Roman <r.nikitin@treolan.ru>
2022-11-03 23:10:30 +0600

    added db project

---
+++ Database/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {}
+    }
+}
---
+++ Resource.Api/Models/LittleNote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Resource.Api.Models
+{
+    public class LittleNote
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Attachments { get; set; }
+        public string Background { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public DateTime Deleted { get; set; }
+        public Guid AccountId { get; set; }
+    }
+}
