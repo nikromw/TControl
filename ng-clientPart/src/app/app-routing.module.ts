61e938baf7e24fd29fd5ebeb852380770478f652
Nikitin Roman <nikromw@mail.ru>
2022-12-15 21:51:17 +0600

    added some styles

--- ng-clientPart/src/app/app-routing.module.ts
+++ ng-clientPart/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AuthComponent } from './components/auth/auth.component';
 import { HomeComponent } from './components/home/home.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ResumeComponent } from './components/resume/resume.component';
 
 const routes: Routes = [
   {path: '', component: AuthComponent},
   {path: 'home', component: HomeComponent},
   {path: 'orders', component: OrdersComponent},
-  {path: 'profile', component: ProfileComponent }
+  {path: 'profile', component: ProfileComponent },
+  {path: 'resume', component: ResumeComponent }
 ];
 
 @NgModule({
